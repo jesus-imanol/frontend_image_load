0838f8a951ee48231ff1d325465f48026254d9f7
jesus-imanol <jesusimanolcastillo@gmail.com>
2024-07-20 23:43:29 -0600

    first commit or initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const UploadForm = () => {
+  const [file, setFile] = useState(null);
+  const [name, setName] = useState('');
+
+  const url = 'http://localhost:8000/api'
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    //Creamos un formData para crear el body de la petición (el resultado será un JSON)
+    const formData = new FormData();
+    //Añadimos los datos que vayamos a guardar, en mi caso nomas un string como name
+    formData.append('name', name);
+    //Añadimos la imagen que obtuvimos del input file
+    formData.append('productImage', file);
+
+    try {
+      const response = await fetch(url+'/products', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="file" onChange={handleFileChange} />
+        <input type="text" value={name} onChange={handleNameChange} placeholder="Name" />
+        <button type="submit">Upload</button>
+      </form>
+      <div>
+        <p>Uploaded file:</p>
+        <img src='http://localhost:8000/uploads/1721360149636-as.jpeg' alt="Uploaded file" style={{ maxWidth: '500px', maxHeight: '500px' }} />
+      </div>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <UploadForm />
+    </div>
+  );
+};
 
 export default App;
